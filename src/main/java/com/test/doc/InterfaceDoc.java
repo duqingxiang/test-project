987918794e344f081ca0ccb1f464b60ea66b3534
duqingxiang <duqingxiang@huobi.com>
2019-08-15 11:19:31 +0800

    bvdfvsds

---
+++ src/main/java/com/test/doc/InterfaceDoc.java
@@ -0,0 +1,42 @@
+package com.test.doc;
+
+public class InterfaceDoc {
+
+//  /write/verify  // h5页面入口链接
+//  参数：验证字符串（我已阅读xxxx）
+//  返回结果：h5页面
+//
+//  /api/write/token/assign  // 验证token分配
+//  参数：验证字符串（我已阅读xxxx）
+//  返回结果：
+//  {
+//    "code": 200,
+//      "data": {
+//    "index": {
+//      0: "A",
+//          1: "B",
+//          2: "C"
+//    },
+//    "token": "xxxeeexososjfod23fsodijxc"
+//  }
+//  }
+//
+//
+//  /api/write/word/verify // 验证输入文字内容
+//  参数：token，index，手写文字图片
+//  返回结果 {
+//    "code": 200 // 200成功， 其他码为失败
+//  }
+//
+//  /api/write/token/get // 验证内容结果获取
+//  参数：token
+//  {
+//    "code": 200,
+//      "data": {
+//    "wordList": ["http://abc.com/pic/1", "http://abc.com/pic/2"], //手写文字图片链接
+//    "token": "xxxeeexososjfod23fsodijxc", // 验证token
+//        "status": 1 // 验证结果
+//  }
+//  }
+
+}
